6f9f31b012059003370db18f5e7291594fce95bb
GreenHex <GreenHex@gmail.com>
2016-12-04 12:16:16 +0530

    Formatting

--- src/c/swap.c
+++ src/c/swap.c
@@ -1,19 +1,19 @@
 //
 // Copyright (c) 2016 Vinodh Kumar M. <GreenHex@gmail.com>
 //
-// From: http://www.geeksforgeeks.org/shuffle-a-given-array/
+// Source: http://www.geeksforgeeks.org/shuffle-a-given-array/
 //
 
 #include <pebble.h>
 #include "swap.h"
 
-static void swap (int *a, int *b) {
+static void swap( int *a, int *b) {
     int temp = *a;
     *a = *b;
     *b = temp;
 }
  
-void randomize ( int arr[], int n ) {
+void randomize( int arr[], int n ) {
     srand( time( NULL ) );
     for ( int i = n-1; i > 0; i-- ) {
         int j = rand() % ( i + 1 );
